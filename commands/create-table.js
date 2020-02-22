39e59503b15eae37dbb06c796d36d4d9e7580516
Leandro Viana <leandrovianacodes@gmail.com>
2020-02-21 20:40:16 -0400

    Adicionando verificação de cargos

--- commands/create-table.js
+++ commands/create-table.js
@@ -2,10 +2,13 @@ module.exports = {
   name: 'create-table',
   description: 'Cria uma tabela!',
   execute(message, args) {
-    if (!args.length) return message.channel.send(`Você não especificou nenhum nome para a mesa, ${message.author}!`)
-    else {
-      const tableName = args.join(' ')
-      message.channel.send(`Você criou uma mesa de RPG com o seguinte nome: ${tableName}`)
-    }
+    let mestreRPG = message.guild.roles.find(role => role.name === 'Mestre')
+    if (message.member.roles.has(mestreRPG.id)) {
+      if (!args.length) return message.channel.send(`Você não especificou nenhum nome para a mesa, ${message.author}!`)
+      else {
+        const tableName = args.join(' ')
+        message.channel.send(`Você criou uma mesa de RPG com o seguinte nome: ${tableName}`)
+      }
+    } else message.channel.send('Me parece que você não pode usar isso...')
   }
 }
--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,10 @@ module.exports = {
   name: 'ping',
   description: 'Ping!',
   execute(message) {
-    message.channel.send('Pong.')
+    let mestreRPG = message.guild.roles.find(role => role.name === 'Esquilo')
+    if (message.member.roles.has(mestreRPG.id))
+        message.channel.send('Pong.')
+    else 
+        message.channel.send('Me parece que você não pode usar isso...')
   }
 }
